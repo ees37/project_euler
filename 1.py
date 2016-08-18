fa16ec5283d79089b1e9557c509023f0ddc14c82
Ethan Schweinsberg <ees37@zips.uakron.edu>
2016-08-17 22:36:43 -0400

    First commit; solutions to two of the problems

---
+++ 1.py
@@ -0,0 +1,20 @@
+# If we list all the natural numbers below 10 that are 
+# multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of 
+# these multiples is 23.
+#
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+# initialize
+import time
+start = time.time()
+result = 0
+
+# do work
+for i in range (0,1000): 
+    if not i % 3 or not i % 5:
+        result = result + i
+
+# show result
+print(result)
+totalMs = (time.time() - start) * 1000
+print("solved in " + str(totalMs) + " ms") 
---
+++ template.py
@@ -0,0 +1,14 @@
+
+
+# initialize 
+import time
+start = time.time()
+result = 0
+
+# do work
+
+# show result
+print(result)
+totalMs = (time.time() - start) * 1000
+print("solved in " + str(totalMs) + " ms")
+
